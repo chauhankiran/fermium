7cf6ee2f616ca9268c1ab3de695633715da0babb
Kiran <kiranchauhan68@gmail.com>
2022-12-05 13:05:37 +0530

    added companies listing page and stub for companies pages

--- controllers/companies.controller.js
+++ controllers/companies.controller.js
@@ -1,7 +1,43 @@
+const { Company } = require("../models");
+
 const companiesController = {};
 
-companiesController.index = (req, res) => {
-  res.render("companies/index.view.html", { data: { title: res.locals.globalAppName }});
+companiesController.index = async (req, res, next) => {
+  try {
+    const companies = await Company.findAndCountAll();
+
+    const data = { title: "Companies", companies: companies.rows, count: companies.count }
+    res.render("companies/index.view.html", { data });
+  } catch (err) {
+    next(err);
+  }
 };
+
+companiesController.add = (req, res) => {
+  res.render("companies/add.view.html", { data: { title: "Add new company" }});
+}
+
+companiesController.edit = (req, res) => {
+  res.render("companies/edit.view.html", { data: { title: "Edit this company" }});
+}
+
+companiesController.show = (req, res) => {
+  res.render("companies/show.view.html", { data: { title: "Show this company" }});
+}
+
+companiesController.create = (req, res) => {
+  res.status(501).send("Not implemented");
+  return;
+}
+
+companiesController.update = (req, res) => {
+  res.status(501).send("Not implemented");
+  return;
+}
+
+companiesController.destroy = (req, res) => {
+  res.status(501).send("Not implemented");
+  return;
+}
 
 module.exports = companiesController;
--- routes/companies.route.js
+++ routes/companies.route.js
@@ -2,8 +2,14 @@ const express = require("express");
 const auth = require("../middleware/auth.middleware");
 const router = express.Router();
 
-const controlles = require("../controllers")
+const controllers = require("../controllers")
 
-router.get("/", auth, controlles.companiesController.index)
+router.get("/", auth, controllers.companiesController.index);
+router.get("/add", auth, controllers.companiesController.add);
+router.get("/:id", auth, controllers.companiesController.show);
+router.get("/:id/edit", auth, controllers.companiesController.edit);
+router.post("/", auth, controllers.companiesController.create);
+router.put("/:id", auth, controllers.companiesController.update);
+router.delete("/:id", auth, controllers.companiesController.destroy);
 
 module.exports = router;
